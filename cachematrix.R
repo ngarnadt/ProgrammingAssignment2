bcfd680d996e57fc1210eaa63c31026fa4dfb16d
ngarnadt <niklas.garnadt@gess.uni-mannheim.de>
2016-02-26 10:33:15 +0100

    Added new files

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    setmatrix <- function(y){
+        x <<- y
+        inv <<- NULL
+    }
+    getmatrix <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(setmatrix = setmatrix,
+         getmatrix = getmatrix,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)){
+            message("getting cached inverse")
+            return(inv)
+        }
+        matrix <- x$getmatrix()
+        inv <- solve(matrix, ...)
+        x$setinverse(inv)
+        inv
 }
